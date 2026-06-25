ed9d6ef1bf0bfe15c37fc7659bafa8595cac1ca2
Alexandru Badiu <alexandru@badiu.me>
2026-06-25 22:56:23 +0100

    ci: upload test results to codecov

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   test: {
     globals: true,
     include: ['src/**/*.test.ts'],
+    reporters: process.env.CI ? ['default', 'junit'] : ['default'],
+    outputFile: { junit: './test-results.junit.xml' },
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
